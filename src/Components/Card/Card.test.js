19183bebb80987851c3047c67b84187c25d31b86
jds-walker <jds_walker@optusnet.com.au>
2018-11-28 08:44:17 +1100

    struggling with enzyme / typescript

--- src/Components/Card/Card.test.js
+++ src/Components/Card/Card.test.js
@@ -1,6 +1,7 @@
 import { shallow, mount, render } from "enzyme";
 import React from "react";
 import Card from "./Card";
+import { string, number } from "prop-types";
 
 it("expect to render Card component", () => {
   expect(shallow(<Card />).length).toEqual(1);
