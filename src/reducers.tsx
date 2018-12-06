b845afe8eabb4a070934d1179b394261f8f8ec2e
jds-walker <jds_walker@optusnet.com.au>
2018-12-06 20:01:57 +1100

    fixed the tsx reduction

--- src/reducers.tsx
+++ src/reducers.tsx
@@ -1,15 +1,18 @@
 import { CHANGE_SEARCH_FIELD } from "./constants";
 import { object, string, any } from "prop-types";
 
-// interface IInitialState {
-//     searchField: string
-// }
+interface IInitialState {
+  searchField: string;
+}
 
 const initialState = {
   searchField: ""
 };
 
-export const searchRobots = (state = initialState, action = {}) => {
+export const searchRobots = (
+  state: IInitialState = initialState,
+  action: any = {}
+) => {
   switch (action.type) {
     case CHANGE_SEARCH_FIELD:
       return Object.assign({}, state, { searchField: action.payload });
