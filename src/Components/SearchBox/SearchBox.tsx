15d9fab04370455ee89c7e91e2ca19693325e02c
jds-walker <jds_walker@optusnet.com.au>
2018-12-11 13:04:37 +1100

    accessability

--- src/Components/SearchBox/SearchBox.tsx
+++ src/Components/SearchBox/SearchBox.tsx
@@ -8,9 +8,10 @@ interface ISearchBoxProps {
 const SearchBox = ({ searchChange }: ISearchBoxProps) => {
   return (
     <nav>
-      <div className="pa2 bg-lightest-blue">
+      <div className="pa2 bg-black">
         <h1 className="f2">RoboFriends</h1>
         <input
+          aria-label="search robots"
           className="pa3 ba b--green bg-lightest-blue"
           type="search"
           placeholder="Search Robots"
